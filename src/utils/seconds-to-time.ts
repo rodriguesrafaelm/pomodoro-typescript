c44e74a0c7b0c96c7d50b2241aaed13d3bf9716e
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2023-03-06 01:32:48 -0300

    update seconds-to-time function

--- src/utils/seconds-to-time.ts
+++ src/utils/seconds-to-time.ts
@@ -1,15 +1,19 @@
-export function secondsToTime(seconds: number): string {
-  const hours = Math.floor(seconds / 3600);
-  const minutes =
-    Math.floor((seconds % 3600) / 60)
-      .toString()
-      .padStart(2, '0') + 'm';
-  const remainingSeconds = (seconds % 60).toString().padStart(2, '0') + 's';
+export function secondsToTime(totalSeconds: number, pattern: string): string {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
 
-  if (hours > 0) {
-    const formatedHours = hours.toString().padStart(2, '0') + 'h';
-    return `${formatedHours} ${minutes} ${remainingSeconds}`;
-  } else {
-    return `${minutes} ${remainingSeconds}`;
+  const formatedHours = hours ? hours.toString().padStart(2, '0') + 'h ' : '';
+  const formatedMins = minutes.toString().padStart(2, '0') + 'm ';
+  const formatedSeconds = (seconds % 60).toString().padStart(2, '0') + 's ';
+
+  if (pattern == 'HMS') {
+    return `${formatedHours}${formatedMins}${formatedSeconds}`;
   }
+  if (pattern == 'HM') {
+    return `${formatedHours}${formatedMins}`;
+  }
+  throw new Error(
+    'Should declare a second argument for pattern "HMS" for hours:minutes:seconds or "HM" for hours:minutes',
+  );
 }
